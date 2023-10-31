a09995711a428e61fd67f575850f5dc277da7654
Ademar Fatama Ruiz <ademarx17@gmail.com>
2023-10-31 11:22:04 -0500

    update

--- lab seaman 4 paralela/GetHeapSize.java
+++
@@ -1,6 +0,0 @@
-public class GetHeapSize {
-    public static void main(String[] args) {
-        long heapsize = Runtime.getRuntime().totalMemory();
-        System.out.println("heapsize is :: " + heapsize);
-    }
-} 
